d69d84eabe64895dfbd95e52febeed8e8a59ee83
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-03-19 12:06:10 -0300

    Estrutura do projeto-Carregando variáveis ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import { app } from "./app";
+import { env } from "./env/endex";
 
 app.listen({
-    port: 3333
+    port: env.PORT,
 }).then(() => {
     console.log("🚀 HTPP Rerver Running 🚀")
 })
