49e41009fb82aa900b3b815d181934d767acdad0
Jefferson Marques <56725964+jefim9413@users.noreply.github.com>
2023-03-22 15:36:58 -0300

    Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Jefferson Marques',
+    email: 'jefferson.jsm3444@gmail.com',
+  },
+})
